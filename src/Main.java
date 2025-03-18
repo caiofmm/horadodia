7967e21c95c92ec28abe31e1e2bc348d89e56f1c
caiofmm <caiofilipemm@gmail.com>
2025-03-18 00:07:22 -0300

    conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("EX mensagem");
+        Mensagem.obterMensagem(5);
+        Mensagem.obterMensagem(16);
+        Mensagem.obterMensagem(19);
     }
 }
